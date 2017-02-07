dff421c371baf99dbeadf378d338a1bf6978c2c1
Pedro Reis Colaço <prcolaco@gmail.com>
2017-02-07 15:54:20 +0000

    Initial commit...

---
+++ app/app.spec.ts
@@ -0,0 +1,7 @@
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  it('should be a function', () => {
+    expect(typeof AppComponent).toBe('function');
+  });
+});
---
+++ app/home/home.component.spec.ts
@@ -0,0 +1,11 @@
+import { HomeComponent } from './home.component';
+
+describe('Home Component', () => {
+  it('should be named `HomeComponent`', () => {
+    expect(HomeComponent['name']).toBe('HomeComponent');
+  });
+
+  it('should have a method called `updateMessage`', () => {
+    expect(HomeComponent.prototype.updateMessage).toBeDefined();
+  });
+});
